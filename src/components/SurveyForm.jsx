889f851e6bb21281f561f69d9af9df0997939b28
hckang17 <hckang17@naver.com>
2023-11-27 16:53:22 +0900

    feat:캘린더 데이터 받아오기 작업 임시

--- src/components/SurveyForm.jsx
+++ src/components/SurveyForm.jsx
@@ -4,6 +4,7 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Modal } from 'react-bootstrap';
+import axios from "axios";
 
 const requiredMessage = `필수 응답란입니다.`;
 const warningStyle = {
@@ -14,7 +15,8 @@ const schema = yup.object().shape({
   questGender: yup.string().required(requiredMessage),
   questOld: yup.string().required(requiredMessage),
   questUsageTerm: yup.string().required(requiredMessage),
-  questUsageTerm: yup.string().required(requiredMessage),
+  questPermTerm: yup.string().required(requiredMessage),
+  questDyeTerm: yup.string().required(requiredMessage),
   questRecommend: yup.string().required(requiredMessage),  
 });
 
@@ -27,26 +29,44 @@ function SurveyForm() {
   });
   // 폼을 작성하기 위한 메서드.
   
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     // 여기서 data에는 모든 컨트롤러의 값이 포함됩니다.
     alert(JSON.stringify(data));
+
+    try {
+      // Axios를 사용하여 백엔드 엔드포인트에 POST 요청을 보냅니다.
+      const response = await axios.post('여러분의_백엔드_엔드포인트', data);
+  
+      // 응답을 필요에 따라 처리합니다.
+      console.log(response.data); // 백엔드에서 반환한 데이터가 있다고 가정합니다.
+  
+      // 선택적으로 성공 메시지를 표시하거나 다른 페이지로 이동할 수 있습니다.
+      alert('설문이 성공적으로 제출되었습니다!');
+    } catch (error) {
+      // 요청 중에 발생한 오류를 처리합니다.
+      console.error('설문 제출 중 오류:', error.message);
+  
+      // 선택적으로 사용자에게 오류 메시지를 표시할 수 있습니다.
+      alert('설문 제출 중 오류가 발생했습니다. 나중에 다시 시도해주세요.');
+    }
+
   };  // '제출' 버튼을 클릭했을때 발생하는 것들.
 
   const handleModalClose = () => setShowModal(false);
 
   useEffect(() => {
-    if (watch('questRecommend') === 'no') {
+    if (watch('questRecommend') === '0') {
       setShowModal(true);
     }
   }, [watch('questRecommend')]);
 
   const handleYesButtonClick = () => {
-    setValue('questRecommend', 'no');
+    setValue('questRecommend', '0');
     setShowModal(false);
   };
 
   const handleNoButtonClick = () => {
-    setValue('questRecommend', 'yes');
+    setValue('questRecommend', '1');
     setShowModal(false);
   };
 
@@ -95,6 +115,7 @@ function SurveyForm() {
         />
         {errors.questGender && <p style={warningStyle}>{errors.questGender.message}</p>}
       </div>
+      <br></br>
 
       <div>
         <p> * 연령대를 알려주세요.</p>
@@ -121,7 +142,8 @@ function SurveyForm() {
           )}/>
         {errors.questOld && <p style={warningStyle}>{errors.questOld.message}</p>}
       </div>
-
+      <br></br>
+      
       <div>
         <p> * 샴푸 사용빈도를 알려주세요.</p>
         <Controller
@@ -145,11 +167,37 @@ function SurveyForm() {
           )}/>
         {errors.questUsageTerm && <p style={warningStyle}>{errors.questUsageTerm.message}</p>}
       </div>
+      <br></br>
+
+      <div>
+        <p> * 파마 주기를 알려주세요.</p>
+        <Controller
+          name="questPermTerm"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <div>
+              <select {...field}>
+                <option value="" disabled>
+                  선택해주세요
+                </option>
+                <option value="파마를 하지 않음">파마를 하지 않음</option>
+                <option value="한달에 1번">한달에 1번</option>
+                <option value="분기(3개월)당 1회">분기(3개월)당 1회</option>
+                <option value="1년에 2회">1년에 2회</option>
+                <option value="1년에 1회">1년에 1번</option>
+                {/* 추가적인 선택 옵션을 필요에 따라 추가할 수 있음 */}
+              </select>
+            </div>
+          )}/>
+        {errors.questPermTerm && <p style={warningStyle}>{errors.questPermTerm.message}</p>}
+      </div>
+      <br></br>
 
       <div>
         <p> * 염색 주기를 알려주세요.</p>
         <Controller
-          name="questPermTerm"
+          name="questDyeTerm"
           control={control}
           defaultValue=""
           render={({ field }) => (
@@ -167,8 +215,9 @@ function SurveyForm() {
               </select>
             </div>
           )}/>
-        {errors.questUsageTerm && <p style={warningStyle}>{errors.questUsageTerm.message}</p>}
+        {errors.questDyeTerm && <p style={warningStyle}>{errors.questDyeTerm.message}</p>}
       </div>
+      <br></br>
 
       <div>
         <p>헤어케어 제품을 추천받으시겠습니까?</p>
@@ -179,8 +228,8 @@ function SurveyForm() {
           render={({ field }) => (
             <div>
               <select {...field}>
-                <option value="yes">추천을 받겠습니다.</option>
-                <option value="no">추천을 받지 않겠습니다.</option>
+                <option value="1">추천을 받겠습니다.</option>
+                <option value="0">추천을 받지 않겠습니다.</option>
                 {/* 추가적인 선택 옵션을 필요에 따라 추가할 수 있음 */}
               </select>
               {/* 추가적인 체크 박스 옵션을 필요에 따라 추가할 수 있음 */}

9d1e8260a636d1bf9b42c9151a07bd1d96afd263
siwoojang <j35857834@gmail.com>
2023-11-27 15:35:18 +0900

    login state fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -15,23 +14,6 @@ import SurveyPage from "./pages/SurveyPage";
 import { AuthProvider } from "./functions/AuthContext";
 import AuthRoute from "./pages/AuthRoute";
 import Empty from "./pages/empty";
-=======
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
-import { useState } from 'react';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
-import WishlistPage from './pages/WishlistPage';
-import Magazine from './pages/Magazine';
-import SignupForm from './pages/Signup';
-import CalendarPage from './pages/CalendarPage';
-import LoginPage from './pages/login';
-import RegisterPage from './pages/register';
-import Register_success from './pages/Register_success';
-import SurveyPage from './pages/Survey';
->>>>>>> efadd12d0bc709c7d7ab53b20ce661782c508f32
 
 function App() {
   return (
@@ -44,7 +26,6 @@ function App() {
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-<<<<<<< HEAD
                 <Route
                   path="/calender"
                   element={
@@ -89,17 +70,6 @@ function App() {
                 <Route path="*" element={<NotFound />} />
               </Routes >
               
-=======
-                <Route path="/calender" element={isLoggedIn ? <CalendarPage/> : <Navigate to="/" />} />
-                <Route path="/magazine" element={isLoggedIn ? <Magazine/> : <Navigate to="/" />} />
-                <Route path="*" element={<NotFound/>} />
-                <Route path="/wishlist" element={isLoggedIn ? <WishlistPage/> : <Navigate to="/" />} />
-                <Route path="/survey" element={<SurveyPage/>} />
-                <Route path="/login" element={<LoginPage setIsLoggedIn={setIsLoggedIn} handleLogout={handleLogout} />} />
-                <Route path="/register" element={<RegisterPage/>}/>
-                <Route path="/register_success" element={<Register_success/>}/>
-              </Routes>
->>>>>>> efadd12d0bc709c7d7ab53b20ce661782c508f32
             </div>
           </div>
         </Router>
